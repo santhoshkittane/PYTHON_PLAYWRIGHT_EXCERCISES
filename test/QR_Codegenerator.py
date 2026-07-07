1e3532ac5d9aa9a152ab4c36c76d0ded8473e104
Santhosh Kittane <santhosh.kittane@accionlabs.com>
2026-07-07 15:25:44 +0530

    adding QR generator Files

--- test/QR_Codegenerator.py
+++ test/QR_Codegenerator.py
@@ -1,12 +1,24 @@
 from qrcode import QRCode
+from datetime import datetime
+import URLMatching
+
+
 
 url = input("Enter URL: ").strip()
-file_path = "C:\\Users\\SanthoshKittane\\Desktop\\PYTHON\\QRCode.png"
+web_name = URLMatching.get_website_name(url)
+print(web_name)
+now = web_name+"-QRCode-"+str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))+".png"
+print(now)
+if URLMatching.urlmatch(url):
+    file_path = "C:\\Users\\SanthoshKittane\\Desktop\\PYTHON\\"+now
 
-qr= QRCode()
-qr.add_data(url)
+    qr= QRCode()
+    qr.add_data(url)
 
-img=qr.make_image()
-img.save(file_path)
+    img=qr.make_image()
+    img.save(file_path)
 
-print("QR Code Generated")
+    print("QR Code Generated")
+
+else:
+    print("Invalid URL")
