676d28b78020458512047b6d901abe9881782691
Santhosh Kittane <santhosh.kittane@accionlabs.com>
2026-07-10 11:52:10 +0530

    new comimt after changing REPO Name

--- String-Excercises/swapcase.py
+++ String-Excercises/swapcase.py
@@ -1,2 +1,2 @@
 inp = input("Enter any String:")
-print(f"{inp} is the input string, after changing case it is {inp.swapcase()}")
+print(f"{inp} is the input strings, after changing case it is {inp.swapcase()}")
